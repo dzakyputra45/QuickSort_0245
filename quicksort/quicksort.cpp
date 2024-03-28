014f0af1e72a3cd2b882bedb64813acfa4ead235
dzakyputra45 <dzakypratama45@gmail.com>
2024-03-28 14:03:40 +0700

    Selesai masukkan data

--- quicksort/quicksort.cpp
+++ quicksort/quicksort.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int arr[20];          //array of interest to hold value
 int cmp_count = 0;   //number of comparasion
 int mov_count;      // number of data movements
+int n;
 
 
 
