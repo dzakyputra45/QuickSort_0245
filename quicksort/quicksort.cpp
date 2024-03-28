3057e0e7b9efe8d1d3e460d1bc7112af76a92b0c
dzakyputra45 <dzakypratama45@gmail.com>
2024-03-28 14:26:40 +0700

    Masukkan pengoperasian data

--- quicksort/quicksort.cpp
+++ quicksort/quicksort.cpp
@@ -1,34 +1,24 @@
 #include <iostream>
 using namespace std;
 
-int arr[20];          //array of interest to hold value
-int cmp_count = 0;   //number of comparasion
+int arr[20];          // Array of integers to hold values
+int cmp_count = 0;   // number of comparisons
 int mov_count;      // number of data movements
-int n;
 
-void input() {
-    while (true)
-    {
-        cout << "Masukkan panjang element array";
+void Input() {
+    int n;
+    while (true) {
+        cout << "Masukkan panjang elemen array: ";
         cin >> n;
         if (n <= 20)
             break;
         else
-            cout << "\nMaksimun panjang array adalah 20" << endl;
+            cout << "\nMaksimum panjang array adalah 20." << endl;
     }
-}
+    cout << "\n" << endl;
+    cout << "\nMasukkan Elemen Array" << endl;
+    cout << "......................" << endl;
 
-
-
-
-
-
-
-
-
-
-
-int main()
-{
-    std::cout << "Hello World!\n";
+    for (int i = 0; i < n; i++) 
+    
 }
