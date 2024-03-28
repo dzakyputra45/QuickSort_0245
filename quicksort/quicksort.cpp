ad826bc648eed95986c544732c0b7eee9956b23a
dzakyputra45 <dzakypratama45@gmail.com>
2024-03-28 14:55:01 +0700

    Mengubah variable i dan j

--- quicksort/quicksort.cpp
+++ quicksort/quicksort.cpp
@@ -48,22 +48,25 @@ void q_short(int low, int high)  // Quick sort algorithm
     i = low + 1;        //step 3
     j = high;           //step 4
 
-    while (i <= j)      //step 10
+    while (i <= j)                                       //step 10
     {
-        while (i <= j) {    //search for a element greater than pivot
+        while (i <= j) {                                //search for a element greater than pivot
             while ((arr[i] <= pivot) && (i <= high))    //step 5
             {
                 i++;                                    //step 6
                 cmp_count++;
             }
-            cmp_count++;    //search for a element less than or equal to pivot
+            cmp_count++;                                //search for a element less than or equal to pivot
             while (arr[j] > pivot && j >= low)          //step 7
             {
                 j--;                                    //step 8
                 cmp_count++;
             }
-
-
+            cmp_count++;
+            if (i < j)                                  //step 9
+            {
+                swap(i, j);                             //swap the element at index o whit the element at index
+            }
 
     }
 
