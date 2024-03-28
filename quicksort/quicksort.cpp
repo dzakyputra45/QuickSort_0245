a0959c2634d798f24b3a6515f9f843b0db941f9b
dzakyputra45 <dzakypratama45@gmail.com>
2024-03-28 14:50:29 +0700

    Mengoperasikan data yang lebih kecil

--- quicksort/quicksort.cpp
+++ quicksort/quicksort.cpp
@@ -51,11 +51,19 @@ void q_short(int low, int high)  // Quick sort algorithm
     while (i <= j)      //step 10
     {
         while (i <= j) {    //search for a element greater than pivot
-            while ((arr[i] <= pivot) && (i <= high)) 
+            while ((arr[i] <= pivot) && (i <= high))    //step 5
             {
-                i++;
+                i++;                                    //step 6
                 cmp_count++;
             }
+            cmp_count++;    //search for a element less than or equal to pivot
+            while (arr[j] > pivot && j >= low)          //step 7
+            {
+                j--;                                    //step 8
+                cmp_count++;
+            }
+
+
 
     }
 
